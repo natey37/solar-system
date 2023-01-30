664ae4579612ac0dad593fbd2e54913eefe82e9f
Nathan Brickett <nathanbrickett@gmail.com>
2023-01-29 21:17:00 -0800

    Add rotation animation to planets

--- src/Planet.jsx
+++ src/Planet.jsx
@@ -1,17 +1,25 @@
-import React, { useRef, useState } from 'react'
-import { useFrame } from '@react-three/fiber'
-import * as THREE from 'three'
+import React, { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 function Planet(props) {
-  const ref = useRef()
+  const ref = useRef();
 
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
 
   useFrame((state, delta) => {
-    ref.current.rotation.x += delta
-    // ref.current.rotation.z += .01
-  })
+    if (props.orbitRadius === 0) return; // don't animate the sun
+    if (!props.isAnimating) return;
+
+    let time = state.clock.getElapsedTime();
+    let orbitSpeed = props.orbitSpeed;
+    let orbitRadius = props.orbitRadius;
+
+    ref.current.position.x = orbitRadius * Math.sin(time * orbitSpeed);
+    ref.current.position.z = orbitRadius * Math.cos(time * orbitSpeed);
+    ref.current.rotation.y += delta * props.rotationSpeed;
+  });
 
   return (
     <mesh
@@ -20,11 +28,12 @@ function Planet(props) {
       scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onPointerOut={(event) => hover(false)}
+    >
       <sphereGeometry args={[props.size, 32, 32]} />
-      <meshStandardMaterial map={new THREE.TextureLoader().load(props.image)}  />
+      <meshStandardMaterial map={new THREE.TextureLoader().load(props.image)} />
     </mesh>
-  )
+  );
 }
 
-export default Planet
+export default Planet;
