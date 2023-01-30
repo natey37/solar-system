6451e1d6e56c4f6a40deff9029b7ee209c52469b
Nathan Brickett <nathanbrickett@gmail.com>
2023-01-29 21:17:53 -0800

    Move planet constants to seperate file

---
+++ src/constants.js
@@ -0,0 +1,35 @@
+export const rotationSpeeds = [
+  0.0039, // Mercury
+  0.0029, // Venus
+  0.0019, // Earth
+  0.0015, // Mars
+  0.0009, // Jupiter
+  0.0007, // Saturn
+  0.0005, // Uranus
+  0.0004, // Neptune
+];
+export const orbitSpeeds = [1.0, 0.9, 0.75, 0.65, 0.5, 0.4, 0.3, 0.2, 0.1];
+export const orbitalRadii = [
+  57.91 / 8,
+  108.2 / 8,
+  149.6 / 8,
+  227.9 / 8,
+  778.5 / 8,
+  1433.5 / 8,
+  2872.5 / 8,
+  4495.1 / 8,
+];
+
+// Sun: 109
+// Jupiter: 11.2
+// Saturn: 9.45
+// Uranus: 4.01
+// Neptune: 3.88
+// Earth: 1
+// Venus: 0.95
+// Mars: 0.53
+// Mercury: 0.38
+
+export const planetSizes = [0.38, 0.95, 1, 0.53, 11.2, 9.45, 4.01, 3.88];
+
+export const planetSizesSmall = [0.38, 0.95, 1, 0.53, 11.2, 9.45, 4.01, 3.88];
